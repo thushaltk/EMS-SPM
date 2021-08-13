ee354430a40208c0bbd7e793bc37ce349956f36f
Thushal Tharaka Kulatilake <thushal1999@gmail.com>
2021-08-14 03:05:43 +0530

    Added landing page + image assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/Header/header.component';
 import { EmpLoginComponent } from './components/Login/EmpLogin/emp-login.component';
+import { LandingPageComponent } from './components/Landingpage/landingpage.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LandingPageComponent,
     HeaderComponent,
     EmpLoginComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [OtherService],
