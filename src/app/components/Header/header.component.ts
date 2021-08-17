04f43422dbc85d5aad10185b00d35f20209f22bd
Thushal Tharaka Kulatilake <thushal1999@gmail.com>
2021-08-18 02:27:11 +0530

    emp login page

--- src/app/components/Header/header.component.ts
+++ src/app/components/Header/header.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit, DoCheck, ChangeDetectionStrategy} from '@angular/core';
 import { OtherService } from 'services/other.service';
 
@@ -10,12 +11,13 @@ import { OtherService } from 'services/other.service';
 export class HeaderComponent implements OnInit {
   clicked: boolean = false;
   toggled: boolean = false;
-  constructor(private otherService: OtherService) {
+  constructor(private otherService: OtherService, private router: Router) {
   }
   ngOnInit(): void {}
 
   clickHere(){
     this.otherService.setClicked(true);
+    this.router.navigate(['login/empLogin']);
   }
 
   OnToggle(){
